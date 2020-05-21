4e7a8aeedfbce12bb56d48788695fae6f379c015
James Carlo Luchavez <mrstudyfirst@gmail.com>
2020-05-21 09:43:59 -0700

    Added first name and last name fields on User model.

---
+++ app/Traits/UUIDTrait.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Traits;
+use Illuminate\Support\Str;
+trait UUIDTrait
+{
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            $post->{$post->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Traits\UUIDTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
